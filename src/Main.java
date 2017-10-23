2a23d0e329dabb16060f21a200bc23e1954be212
Marvin <Fractal@Marvins-MacBook-Pro.local>
2017-10-22 21:32:28 -0700

    updated main

--- src/Main.java
+++ src/Main.java
@@ -1,228 +1,125 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileFilter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
- 
-public class Main {
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Create extends Repository{
 	
-	//	Runs main program
-	//	Accepts user input of a src and target path to execute repo cloning
-	public static void main(String[] args) {
-		
-		try {
-			repoMenu();
-		} catch (RepoException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+	public Create(String src, String target) {
+		super(src, target);
 	}
-	
-	public static void addLabel(File manifest, String label) throws IOException, RepoException{
-		
-		BufferedReader br = new BufferedReader(new FileReader(manifest));
-		StringBuilder sb = new StringBuilder();
-		int count = 0;
-		
-		while((br.ready())&&(count < 4)){
-			String labelLine = br.readLine().trim();
+
+	/*
+	 * Initiates the ptree for the repository by copying the root folder from src and storing it in target
+	 */
+	@Override
+	public void execute() throws RepoException, IOException{
+		if ((target != "")&&(src != "")){
+			File rootSrc = new File(src);
+			System.out.println("source::"+src); //delete me
+			String rootRep = target + File.separator + rootSrc.getName();
+			System.out.println("Rootrep::"+rootRep); //delete me
+			File rep = new File(rootRep);
 			
-			if (labelLine.isEmpty()){
-				sb.append(label + "\r\n");
-				break; //added label
+			if (rep.mkdir()){
+				duplicate(src, rootRep);
 			}else{
-				sb.append(labelLine + "\r\n");
+				throw new RepoException("Repo already exists!");
 			}
-			count++;
 		}
-		
-		if (count >= 4){
-			br.close();
-			throw new RepoException("Exceed label limit of 4!");
-		}
-		
-		while(br.ready()){
-			String line = br.readLine().trim();
-			sb.append(line + "\r\n");
-		}
-		br.close();
-		BufferedWriter bw = new BufferedWriter(new FileWriter(manifest));
-		bw.write(sb.toString());
-		bw.close();
 	}
 	
-	public static void repoMenu() throws RepoException, IOException
-	{
-		@SuppressWarnings("resource")
-		Scanner kb = new Scanner(System.in);
-		boolean finished = false;
+	/*
+	 * Creates the folders and files for the rest of the ptree for the repository. Uses recursion to traverse the ptree
+	 * @param directory - the string path of the current leaf of the ptree traversal from the source to be copied from
+	 * @param rep - the string path of the current leaf of the ptree traversal from the target to be stored in 
+	 */
+	private void duplicate(String directory, String rep) throws IOException{
+		File dir = new File(directory);
+		File[] listFile = dir.listFiles();
 		
-		while(!finished)
-		{
-			System.out.println("\nRepository Commands\n");
-			System.out.println("1. create-repo [source folder] [target folder]");
-			System.out.println("2. check-in [source folder] [target folder]");
-			System.out.println("3. check-out [source folder] [target folder]");
-			System.out.println("4. label-manifest [manifest file] [label]");
-			System.out.println("5. exit-menu");
-			System.out.println("Enter the command and arguments you would like to perform:");
-			String command = kb.next();
-			System.out.println("Command: " + command);
-			
-			if(command.equals("exit-menu"))
-			{
-				System.out.println("Exiting menu.");
-				finished = true;
-			}
-			else if(command.equals("label-manifest"))
-			{
-				String manifestDir = kb.next();
-				String label = kb.next();
-				kb.nextLine();
-				//	TODO	: create manifest label function
-				File manifest = new File(manifestDir);
-				addLabel(manifest, label);
-			}
-			else
-			{
-				System.out.print(">");
-				String src = kb.next();
-				System.out.print("\n>");
-				String target = kb.next();
-				kb.nextLine();
+		for(File f : listFile){
+			if (f.isDirectory()){
+				String newTarget = rep + File.separator + f.getName();
+				File newDir = new File(newTarget);
+				newDir.mkdir();
+				duplicate(f.getAbsolutePath(), newTarget);
+			}else if (f.isFile()){
+				String newName = aid(f);
+				String newTarget = rep + File.separator + f.getName();
+				File newDir = new File(newTarget);
+				newDir.mkdir();
 				
-				if(command.equals("create-repo"))
-				{
-					Repository rep = new Create(src, target);
-					rep.execute();
-					System.out.println("repo created");
-					
-					//Generate Manifest File
-					String manifestDir = target + File.separator + "manifest.mani";
-					File manifest = new File(manifestDir);
-					rep.generateManifest(manifest);
-					
-					System.out.println("Enter label: ");
-					String label = kb.nextLine();
-					addLabel(manifest, label);
+				File newFile = new File(newTarget + File.separator + newName);
+				InputStream in = new FileInputStream(f);
+				OutputStream out = new FileOutputStream(newFile);
+				byte[] buffer = new byte[1024];
+				int length = in.read(buffer);
+				
+				while (length > 0){
+					out.write(buffer, 0, length);
+					length = in.read(buffer);
 				}
-				else if (command.equals("check-in"))
-				{
-					CheckIn checkin = new CheckIn(src, target);
-					checkin.execute();
-				}
-				else if (command.equals("check-out"))
-				{
-					File repo = new File(src);
-					
-					File[] maniFileList = repo.listFiles(new FileFilter() {
-						@Override
-						public boolean accept(File name) {
-							return name.getName().endsWith(".mani");
-						}
-					});
-					
-					System.out.println("List of manifest files");
-					for(File f: maniFileList)
-					{
-						System.out.println(f.getName());
-					}
-					
-					//	Get manifest file/label from user
-					System.out.println("Enter a manifest file (or label) to check-out from: ");
-					String input = kb.nextLine();
-					
-					//	Determine if input is mainfest file or label
-					//	and retrieve manifest file if label
-					if(!input.isEmpty())
-					{
-						String[] maniOrLabel = input.split("\\.");
-						for(String s: maniOrLabel)
-							System.out.println("Token: " + s);
-						
-						if(maniOrLabel.length > 1 && input.split("\\.")[1].equals("mani"))
-						{
-							boolean maniExists = false;
-							for(File f: maniFileList)
-							{
-								if(f.getName().equals(input))
-									maniExists = true;
-							}
-							
-							if(maniExists)
-							{
-								File maniFile = new File(src + File.separator + input);
-								CheckOut checkout = new CheckOut(src, target, maniFile);
-								checkout.execute();
-							}
-							else
-							{
-								System.out.println("Manifest file does not exist.");
-							}
-						}
-						//	Find manifest file that corresponds with the label given
-						else
-						{
-							String maniName = getManifest(input, maniFileList);
-							if(maniName == null)
-								System.out.println("Label not found.");
-							else
-							{
-								System.out.println(src + File.separator + maniName);
-								File manifest = new File(src + File.separator + maniName);
-								CheckOut checkout = new CheckOut(src, target, manifest);
-								checkout.execute();
-							}
-						}
-					}
-					else
-					{
-						System.out.println("No manifest specified.\n"
-								+ "You must provide a manifest file or label to check out from.");
-					}
-				}
-				else {
-					System.out.println("Invalid command");
-				}
+				
+				in.close();
+				out.close();
 			}
-	
 		}
 	}
-	
-	public static String getManifest(String label, File[] maniList)
-	{
-		for(File mani: maniList)
-		{
-			Scanner read;
-			try {
-				read = new Scanner(mani);
-				int lineCount = 0;
-				while(read.hasNextLine() && lineCount < 4)
-				{
-					if(read.nextLine().equals(label))
-					{
-						return mani.getName();
-					}
-				}
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+
+	@Override
+	public void generateManifest(File manifest) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedWriter bw = new BufferedWriter(new FileWriter(manifest, true));
+		bw.newLine(); //First four line is for labels
+		bw.newLine();
+		bw.newLine();
+		bw.newLine();
+		bw.write("Command: create-repo\r\n");
+		bw.write("Time repo created: " + ZonedDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME) + "\r\n");
+		bw.write("User command: create-repo " + src + " " + target +"\r\n");
+		bw.write("Source path: " + src + "\r\n");
+		bw.write("Target path: " + target + "\r\n");
 		
-		return null;
+		File srcFile = new File(src);
+		File dir = new File(target + File.separator + srcFile.getName());	
+		findFiles(dir, manifest.getAbsolutePath(), bw);
+		bw.close();
+		listManifest.add(manifest);
 	}
+	
+	//	Recursively travel through the directory
+	//	to write info for each file copied into the manifest file.
+	private void findFiles(File dir, String manifestDir, BufferedWriter bw) throws IOException{
+			//Get list of files in directory ignoring the .DS_Store file
+			File[] fileList = dir.listFiles(new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String name) {
+					return !name.equals(".DS_Store");
+				}
+			});
+			
+			for(File f: fileList)
+			{
+				if(f.isDirectory())
+					findFiles(f, manifestDir, bw);
+				else if(f.isFile())
+				{
+					String aid = f.getName();
+					String parent = f.getParentFile().getName();
+					String path = f.getPath();
+					String s = "File Copied Info: " + aid + " " + parent + " " + path + "\r\n";
+					bw.write(s);
+				}
+			}
+	}
+
 }
+
